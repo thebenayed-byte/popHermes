fe36d753e64e529df3ea1b2e6b53bc4f19a584e4
thebenayed-byte <thebenayed@gmail.com>
2026-02-10 21:56:24 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,11 +9,18 @@ const io = new Server(server);
 app.use(express.static(__dirname));
 
 io.on("connection", socket => {
-  socket.on("message", msg => {
-    io.emit("message", msg);
+  console.log("Un utilisateur connecté");
+
+  socket.on("message", (msg) => {
+    console.log("Message reçu:", msg); // <-- utile pour debug
+    io.emit("message", msg); // renvoie à tous
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Utilisateur déconnecté");
   });
 });
 
 server.listen(3000, () => {
-  console.log("http://localhost:3000");
+  console.log("Serveur lancé sur http://localhost:3000");
 });
