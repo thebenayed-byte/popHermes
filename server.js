1fc533b7af104ab579675936362eae11a034fc2a
thebenayed-byte <thebenayed@gmail.com>
2026-02-10 21:59:42 +0300

    Clean up console logs in server.js
    Removed debug logs for user connection and disconnection.

--- server.js
+++ server.js
@@ -9,18 +9,11 @@ const io = new Server(server);
 app.use(express.static(__dirname));
 
 io.on("connection", socket => {
-  console.log("Un utilisateur connecté");
-
-  socket.on("message", (msg) => {
-    console.log("Message reçu:", msg); // <-- utile pour debug
-    io.emit("message", msg); // renvoie à tous
-  });
-
-  socket.on("disconnect", () => {
-    console.log("Utilisateur déconnecté");
+  socket.on("message", msg => {
+    io.emit("message", msg);
   });
 });
 
 server.listen(3000, () => {
-  console.log("Serveur lancé sur http://localhost:3000");
+  console.log("http://localhost:3000");
 });
